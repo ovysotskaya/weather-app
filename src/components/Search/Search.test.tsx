ee7ab5a6addee1ab7a6e6fc78908c9c3938d81cb
ovysotskaya <o2.vysotskaya@itransition.com>
2023-06-01 23:28:15 +0200

    add tests for Search component

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -6,7 +6,8 @@ import store from "../../redux/store";
 import {Provider} from "react-redux";
 import {QueryClient, QueryClientProvider} from "react-query";
 
-test('Type city name and check suggestions', async () => {
+test('Type empty value and check suggestions', async () => {
+
     render(
         <Provider store={store}>
             <QueryClientProvider client={new QueryClient()}>
@@ -15,7 +16,58 @@ test('Type city name and check suggestions', async () => {
         </Provider>
     )
 
-    userEvent.type(screen.getByTestId('geosearch'), " ")
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act( async () => {
+        userEvent.type(screen.getByTestId('geosearch'), " ")
+    })
 
-    //expect(screen.getByText("No locations")).toBeInTheDocument()
+    expect(screen.getByText("No locations")).toBeVisible()
+})
+
+test('Type city name and check suggestions', async () => {
+
+    render(
+        <Provider store={store}>
+            <QueryClientProvider client={new QueryClient()}>
+                <Search/>
+            </QueryClientProvider>
+        </Provider>
+    )
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act( async () => {
+        userEvent.type(screen.getByTestId('geosearch'), "Minsk")
+    })
+
+    const listItems = await screen.findAllByRole("option")
+
+    expect(listItems.length).toBe(1)
+    expect(listItems[0]).toHaveTextContent("Minsk, , BY")
+})
+
+test('Type and select unique city', async () => {
+
+    render(
+        <Provider store={store}>
+            <QueryClientProvider client={new QueryClient()}>
+                <Search/>
+            </QueryClientProvider>
+        </Provider>
+    )
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act( async () => {
+        userEvent.type(screen.getByTestId('geosearch'), "Minsk")
+    })
+
+    const listItems = await screen.findAllByRole("option")
+
+    expect(listItems.length).toBe(1)
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act( async () => {
+        userEvent.click(listItems[0])
+    })
+
+    expect(screen.queryByRole("option")).toBeNull()
 })
